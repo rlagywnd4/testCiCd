ed52daefe94baddc25fdfd35d78d41e8bc69ec5e
rlagywnd4 <rlagywnd4@gmail.com>
2025-02-14 14:21:26 +0900

    first commit

---
+++ src/test/java/com/sparta/testcicd/TestcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.testcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
