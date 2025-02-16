7c027a4205f192bf96ca3698afb2496fd171975b
rlagywnd4 <rlagywnd4@gmail.com>
2025-02-16 20:59:34 +0900

    add aws rds

--- src/test/java/com/sparta/testcicd/TestcicdApplicationTests.java
+++ src/test/java/com/sparta/testcicd/TestcicdApplicationTests.java
@@ -3,11 +3,21 @@ package com.sparta.testcicd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootTest
 class TestcicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testDot(){
+		System.out.println("start-------------");
+		Dotenv dotenv = Dotenv.load();
+		System.out.println("RDS_HOSTNAME: " + dotenv.get("RDS_HOSTNAME"));
+		System.out.println("RDS_PORT: " + dotenv.get("RDS_PORT"));
 	}
 
 }
